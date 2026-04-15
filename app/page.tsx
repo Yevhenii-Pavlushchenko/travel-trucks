2bbd30a6bbbdce1dcbe4bdff12b24824db4b95b3
JustFun <paw93@ukr.net>
2026-04-15 16:44:00 +0300

    #4
    page ome finish

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ import css from "./page.module.css";
 export default function Home() {
   return (
     <section className={css.hero}>
-      <h1>Campers of your dreams</h1>
-      <p>You can find everything you want in our catalog</p>
+      <div className={css.container}></div>
+      <h1 className={css.title}>Campers of your dreams</h1>
+      <p className={ css.descr}>You can find everything you want in our catalog</p>
       <Button text="View Now" color='green'width={173} ></Button>
       
    </section>
