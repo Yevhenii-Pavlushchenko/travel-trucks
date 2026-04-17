3919974ab0a8682e15b1e4406ba0989fa9626f47
JustFun <paw93@ukr.net>
2026-04-17 10:35:16 +0300

    fixed  api

--- lib/api.ts
+++ lib/api.ts
@@ -11,13 +11,17 @@ interface fetchCampersResponse{
 
 }
 
-export const fetchCampers = async ({pageParam = 1}): Promise<fetchCampersResponse> => {
- try {
-     const { data } = await instance.get<{ campers: Camper[]; total: number }>(
-         `/campers?page=${pageParam}&limit=4`);
-    return data; 
+export const fetchCampers = async ({ pageParam = 1 }): Promise<fetchCampersResponse> => {
+  try {
+    const { data } = await instance.get<fetchCampersResponse>("/campers", {
+      params: {
+        page: pageParam,
+        perPage: 4, 
+      },
+    });
+    return data;
   } catch (error) {
     console.error("Error fetching campers:", error);
-     return { campers:[], total:0};
+    throw error;
   }
 };
