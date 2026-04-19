d64cc21d783d10faee353991f252809e5b1a1acd
JustFun <paw93@ukr.net>
2026-04-19 11:19:28 +0300

    add styles info

--- components/CamperDetails/CamperDetails.tsx
+++ components/CamperDetails/CamperDetails.tsx
@@ -4,21 +4,28 @@ import { Camper } from "@/types/camper";
 export default function CamperDetails({ camper }: { camper: Camper }) {
   return (
     <div className={css.infoWraper}>
-      <h1 className={css.title}>{camper.name}</h1>
-      
       <div className={css.meta}>
-        <div className={css.rating}>
-          <svg width="16" height="16"><use href="/sprite.svg#icon-star"></use></svg>
-          <span>{camper.rating}({camper.totalReviews} Reviews)</span>
+        <h1 className={css.title}>{camper.name}</h1>
+        <div className={css.raringWraper}>
+          <div className={css.rating}>
+            <svg  className={css.ratingIcon} width="16" height="16">
+              <use href="/sprite.svg#icon-star"></use>
+            </svg>
+            <span className={css.ratingText} >
+              {camper.rating}({camper.totalReviews} Reviews)
+            </span>
+          </div>
+          <div className={css.location}>
+            <svg width="16" height="16">
+              <use href="/sprite.svg#icon-map"></use>
+            </svg>
+            <span>{camper.location}</span>
+          </div>
         </div>
-        <div className={css.location}>
-          <svg width="16" height="16"><use href="/sprite.svg#icon-map"></use></svg>
-          <span>{camper.location}</span>
-        </div>
-      </div>
 
-      <p className={css.price}>€{camper.price.toFixed(2)}</p>
-      <p className={css.text}>{camper.description}</p>
+        <p className={css.price}>€{camper.price.toFixed(2)}</p>
+        <p className={css.description}>{camper.description}</p>
+      </div>
     </div>
   );
 }
--- types/camper.ts
+++ types/camper.ts
@@ -2,6 +2,11 @@ export type CamperForm = "integrated" | "panel_van" | "alcove"| "semi_integrated
 export type CamperTransmission = "automatic" | "manual";
 export type CamperEngine = "diesel" | "petrol" | "hybrid" | "electric";
 
+export interface GalleryItem {
+  thumb: string;
+  original: string;
+}
+
 export interface Camper {
   id: string;
   name: string;
@@ -19,6 +24,6 @@ export interface Camper {
   engine: CamperEngine;
     amenities: string[]; 
     coverImage: string;
-  gallery?: { thumb: string; original: string }[];
+  gallery?: GalleryItem[]
   totalReviews: number;       
 }
